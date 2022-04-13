856d2e8d9d7b872965e8a26d3ad410dfa1748e8c
Divyansh <96115798+Divyansh123456789@users.noreply.github.com>
2022-04-13 16:50:14 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,49 @@
+img = "";
+status = "";
+objects = [];
+
+function preload() {
+    img = loadImage('dog_cat.jpg');
+}
+
+function setup() {
+    canvas = createCanvas(640, 420);
+    canvas.center();
+
+    objectDetector = ml5.objectDetector('cocossd', ModelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+}
+
+function ModelLoaded() {
+    console.log("Model Loaded!");
+    status = true;
+    objectDetector.detect(img, gotResult);
+
+}
+
+function draw() {
+    image(img, 0, 0, 640, 420);
+
+    if (status != "") {
+        for (i = 0; i < objects.length; i++) {
+            document.getElementById("status").innerHTML = "Status: Object detected";
+
+            fill("FF0000");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y +15);
+            noFill();
+            stroke("FF0000");
+            rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+
+        }
+    }
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.log(error);
+
+    }
+    console.log(results);
+    objects = results;
+}
